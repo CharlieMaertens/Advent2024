83a69c0e8d952e00829b29a82391f459da9c33db
CharlieMaertens <134021647+CharlieMaertens@users.noreply.github.com>
2024-12-02 23:24:17 -0600

    day1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -17,45 +17,69 @@ func check(e error) {
 }
 
 func buildLists() ([]int, []int) {
-	myfile, err := os.Open("input.txt")
+	file, err := os.Open("input.txt")
 	check(err)
-	defer myfile.Close()
+	defer file.Close()
 
-	lst1 := []int{}
-	lst2 := []int{}
+	l1 := []int{}
+	l2 := []int{}
 
-	scanner := bufio.NewScanner(myfile)
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		numbers := strings.Split(line, "   ")
-		num1, err := strconv.Atoi(numbers[0])
+		nums := strings.Split(line, "   ")
+		num1, err := strconv.Atoi(nums[0])
 		check(err)
-		num2, err := strconv.Atoi(numbers[1])
+		num2, err := strconv.Atoi(nums[1])
 		check(err)
 
-		lst1 = append(lst1, num1)
-		lst2 = append(lst2, num2)
+		l1 = append(l1, num1)
+		l2 = append(l2, num2)
 	}
 
 	check(scanner.Err())
-	return lst1, lst2
+	return l1, l2
 }
 
-func solve() float64 {
-	lst1, lst2 := buildLists()
+func solvePart1() int {
+	l1, l2 := buildLists()
 
-	sort.Ints(lst1)
-	sort.Ints(lst2)
+	sort.Ints(l1)
+	sort.Ints(l2)
 
-	answer := 0.0
-	for i := 0; i < len(lst1); i++ {
-		answer += math.Abs(float64(lst1[i] - lst2[i]))
+	a := 0.0
+	for i := 0; i < len(l1); i++ {
+		a += math.Abs(float64(l1[i] - l2[i]))
 	}
 
-	return answer
+	return int(a)
+}
+
+func occurencesMap(nums []int) map[int]int {
+	m := make(map[int]int)
+	for _, num := range nums {
+		m[num]++
+	}
+	return m
+}
+
+func solvePart2() int {
+	l1, l2 := buildLists()
+
+	om := occurencesMap(l2)
+
+	a := 0
+	for _, num := range l1 {
+		a += (num * om[num])
+	}
+
+	return a
 }
 
 func main() {
-	answer := solve()
-	fmt.Printf("%f\n", answer)
+	a := solvePart1()
+	fmt.Printf("%d\n", a)
+
+	a = solvePart2()
+	fmt.Printf("%d\n", a)
 }
